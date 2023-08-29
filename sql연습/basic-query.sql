de3c11bf8ed991d9f87d2482266866ae77f402f0
yyhjin <wendy0301@naver.com>
2023-08-29 16:23:49 +0900

    select 기본

--- sql연습/basic-query.sql
+++ sql연습/basic-query.sql
@@ -35,3 +35,39 @@ select * from pet;
 update pet set name='성타니' where name='성탄이';
 
 -- delete: DML(D)
+delete from pet where name='성타니';
+
+-- load data
+load data local infile 'C:/pet.txt' into table pet;
+update pet set death = null where name != 'bowser';
+
+-- select 연습
+-- 문1) bowser의 주인 이름?
+select owner from pet where name='bowser';
+
+-- 문2) 1998 이후에 태어난 애들은?
+select * from pet where birth >=  '1998-01-01';
+
+-- 문3) 종이 뱀이거나 새인 애들은?
+select * from pet where species = 'snake' or species = 'bird';
+
+-- 예4) order by ~ [asc]
+select name, birth from pet order by birth asc;
+
+-- 예5) order by ~ desc
+select name, birth from pet order by birth desc;
+
+-- 예6) where절에 null 다루기
+select name, birth, death from pet where death is null;
+select name, birth, death from pet where death is null;
+
+-- 예7) like 검색(패턴검색)
+select name from pet where name like 'b%';
+select name from pet where name like '%fy';
+select name from pet where name like '%w%';
+select name from pet where name like '____';
+select name from pet where name like 'b____';
+
+-- 예8) 집계: count, avg, sum, max, min, ...
+select count(*) from pet;
+select max(birth) from pet;  # select절에 집계 함수가 있으면 다른 컬럼을 함께 사용할 수 없음
---
+++ sql연습/select-기본.sql
@@ -0,0 +1,17 @@
+--
+-- select 연습
+--
+
+-- 예제1: departments 테이블의 모든 데이터를 출력
+select * from departments;
+
+-- 프로젝션(projection)
+-- 예제2: employees 테이블에서 직원 이름, 성별, 입사일을 출력
+select first_name as '이름', 
+		gender as '성별', 
+        hire_date as '입사일'
+from employees;
+
+-- distinct
+-- 예제1: titles 테이블에서 모든 직급을 출력하세요
+select distinct title from titles;
