2bc057ee6a641f2f24779566bdea9a083c833bf3
yyhjin <wendy0301@naver.com>
2023-09-01 11:06:02 +0900

    dml 연습

--- sql연습/ddl-dml-연습.sql
+++ sql연습/ddl-dml-연습.sql
@@ -37,3 +37,26 @@ desc member;
 -- dml
 --
 
+-- insert
+insert into member values(null, 'kickscar@gmail.com', password('1234'), '안대혁', '개발팀', null);
+select * from member;
+
+insert into member(no, email, password, name, dept, self_intro) values(null, 'kickscar2@gmail.com', password('1234'), '안대혁2', '개발팀2', null);
+select * from member;
+
+insert into member(email, password, name, dept) values ('kickscar3@gmail.com', password('1234'), '안대혁3', '개발팀3');
+select * from member;
+
+insert into member(email, name, dept, password) values('kickscar4@gmail.com', '안대혁4', '개발팀4', password('1234'));
+select * from member;
+
+
+-- update
+update member
+set email='kixxcar@gmail.com', name='킥스카'
+where no = 4;
+select * from member;
+
+-- delete
+delete from member where no = 4;
+select * from member;
