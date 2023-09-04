b4359acae2c4a0a8fdc68bb75f6732c865752a69
yyhjin <wendy0301@naver.com>
2023-09-04 09:27:57 +0900

    Driver 작동 원리

---
+++ jdbc-practices/src/main/java/driver/MyDriverTest.java
@@ -0,0 +1,38 @@
+package driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDriverTest {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		
+		try {
+			// 1. JDBC Driver Class 로딩
+			Class.forName("driver.MyDriver");
+			
+			// 2. 연결하기
+			String url = "jdbc:mariadb://127.0.0.1:3306/mydb";
+			conn = DriverManager.getConnection(url, "webdb", "webdb123");
+			
+			System.out.println("연결성공! - " + conn);
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: " + e);
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if(conn != null) {
+					conn.close();					
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
